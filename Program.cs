92d2570ac7e26412d19e477e8566d03aa5f82808
Djuli Coelho <126629987+DjuliCoelho@users.noreply.github.com>
2023-10-02 20:15:53 -0300

    Prova

--- Program.cs
+++ Program.cs
@@ -1,281 +1,195 @@
-﻿namespace Lista3
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Prova1
 {
-	class Voo
-	{
-		public int      Id;
-		public DateTime HorarioPartida;
-		public string   LocalDestino;
-		public int      TotalAssentos;
-		public int      AssentosOcupados;
-		
-		public Voo(int id, DateTime horarioPartida, string localDestino, int totalAssentos)
-		{
-			Id                  = id;
-			HorarioPartida      = horarioPartida;
-			LocalDestino        = localDestino;
-			TotalAssentos       = totalAssentos;
-			AssentosOcupados    = 0;
-		}
-	}
-	
-	class Passageiro
-	{
-		public int    Id;
-		public string Nome;
-		
-		public Passageiro(int id, string nome)
-		{
-			Id   = id;
-			Nome = nome;
-		}
-	}
-	
-	class Reserva
-	{
-		public int Id;
-		public int IdPassageiro;
-		public int IdVoo;
-		
-		public Reserva(int id, int idPassageiro, int idVoo)
-		{
-			Id           = id;
-			IdPassageiro = idPassageiro;
-			IdVoo        = idVoo;
-		}
-	}
-	
-	class Aeroporto
-	{
-		public int              UniqueId;
-		public List<Voo>        Voos;
-		public List<Passageiro> Passageiros;
-		public List<Reserva>    Reservas;
-		
-		public Aeroporto()
-		{
-			UniqueId     = 0;
-			Voos         = new List<Voo>();
-			Passageiros  = new List<Passageiro>();
-			Reservas     = new List<Reserva>();
-		}
-		
-		public int GetUniqueId()
-		{
-			UniqueId++;
-			return UniqueId;
-		}
-		
-		public Passageiro GetPassageiroPorId(int id)
-		{
-			foreach(var passageiro in Passageiros)
-			{
-				if(passageiro.Id == id)
-				{
-					return passageiro;
-				}
-			}
-			throw new Exception("passageiro com id " + id + " nao encontrado");
-		}
-		
-		public Passageiro GetPassageiroPorNome(string nome)
-		{
-			foreach(var passageiro in Passageiros)
-			{
-				if(passageiro.Nome == nome)
-				{
-					return passageiro;
-				}
-			}
-			throw new Exception("passageiro com nome " + nome + " nao encontrado");
-		}
-		
-		public Voo GetVooPorId(int id)
-		{
-			foreach(var voo in Voos)
-			{
-				if(voo.Id == id)
-				{
-					return voo;
-				}
-			}
-			throw new Exception("voo com id " + id + " nao encontrado");
-		}
-		
-		public Voo GetVooPorHorario(DateTime horario)
-		{
-			foreach(var voo in Voos)
-			{
-				if(voo.HorarioPartida == horario)
-				{
-					return voo;
-				}
-			}
-			throw new Exception("nenhum voo programado para " + horario.ToString());
-		}
-		
-		public void CadastrarPassageiro(string nome)
-		{
-			var passageiro = new Passageiro(GetUniqueId(), nome);
-			Passageiros.Add(passageiro);
-		}
-		
-		public void CadastrarVoo(DateTime horarioPartida, string localDestino, int totalAssentos)
-		{
-			var voo = new Voo(GetUniqueId(), horarioPartida, localDestino, totalAssentos);
-			Voos.Add(voo);
-		}
-		
-		public void ReservarVoo(string nome, DateTime horario)
-		{
-			var passageiro = GetPassageiroPorNome(nome);
-			var voo = GetVooPorHorario(horario);
-			if(voo.AssentosOcupados == voo.TotalAssentos)
-			{
-				throw new Exception("o voo agendado para " + voo.HorarioPartida + " nao possui mais assentos disponiveis");
-			}
-			voo.AssentosOcupados++;
-			var reserva = new Reserva(GetUniqueId(), passageiro.Id, voo.Id); 
-			Reservas.Add(reserva);
-		}
-		
-		public string ListarReservas()
-		{
-			string output = "";
-			foreach(var reserva in Reservas)
-			{
-				var passageiro = GetPassageiroPorId(reserva.IdPassageiro);
-				var voo = GetVooPorId(reserva.IdVoo);
-				output += passageiro.Nome + ", " + voo.HorarioPartida.ToString() + ", " + voo.LocalDestino + ", " + voo.AssentosOcupados + "/" + voo.TotalAssentos + "\n";
-			}
-			return output;
-		}
-		
-		public void CancelarVoo(DateTime horario)
-		{
-			 try
+    class Quiosque
     {
-        // Encontre o voo a ser cancelado com base no horário
-        Voo vooCancelado = GetVooPorHorario(horario);
+        public string Empresa;
+        public bool ServeBebida;
+        public bool ServeSalgado;
+        public bool ServeDoce;
+        public string Horario;
 
-        // Remove o voo da lista de voos
-        Voos.Remove(vooCancelado);
-
-        // Encontre outros voos com o mesmo destino
-        var voosComMesmoDestino = Voos.FindAll(v => v.LocalDestino == vooCancelado.LocalDestino);
-
-        // Encontre todas as reservas associadas ao voo cancelado
-        var reservasDoVooCancelado = Reservas.FindAll(r => r.IdVoo == vooCancelado.Id);
-
-        // Mova os passageiros das reservas do voo cancelado para os próximos voos com o mesmo destino
-        foreach (var reserva in reservasDoVooCancelado)
+        public Quiosque(string empresa, bool serveBebida, bool serveSalgado, bool serveDoce, string horario)
         {
-            var passageiro = GetPassageiroPorId(reserva.IdPassageiro);
-            foreach (var voo in voosComMesmoDestino)
+            Empresa = empresa;
+            ServeBebida = serveBebida;
+            ServeSalgado = serveSalgado;
+            ServeDoce = serveDoce;
+            Horario = horario;
+        }
+    }
+
+    class Bloco
+    {
+        public string Cor;
+        public string AlimentoPreferido;
+        public string Horario;
+        public int NumeroQuiosques;
+
+        public Bloco(string cor, string alimentoPreferido, string horario)
+        {
+            Cor = cor;
+            AlimentoPreferido = alimentoPreferido;
+            Horario = horario;
+            NumeroQuiosques = 0;
+        }
+    }
+
+    class Alocacao
+    {
+        public string EmpresaQuiosque;
+        public string CorBloco;
+
+        public Alocacao(string empresaQuiosque, string corBloco)
+        {
+            EmpresaQuiosque = empresaQuiosque;
+            CorBloco = corBloco;
+        }
+    }
+
+    class Controle
+    {
+        public List<Quiosque> Quiosques;
+        public List<Bloco> Blocos;
+        public List<Alocacao> Alocacoes;
+
+        public Controle()
+        {
+            Quiosques = new List<Quiosque>();
+            Blocos = new List<Bloco>();
+            Alocacoes = new List<Alocacao>();
+        }
+
+        //(0.5 pontos)
+        //retorna numero de quiosques que abrem apenas a noite
+       public int GetNumeroQuiosquesApenasNoite()
+        {
+            return Quiosques.Count(q => q.Horario == "Noite"); //retornando os Quiosques(q)
+        }
+
+        //(0.5 pontos)
+        //retorna string com os nomes dos quiosques (separados por vírgula) que servem salgados pela manhã
+        public string GetEmpresasSalgadoManha()
+        {
+            var quiosquesSalgadoManha = Quiosques.Where(q => q.Horario == "Manha" && q.ServeSalgado).Select(q => q.Empresa);
+            return string.Join(", ", quiosquesSalgadoManha);
+        }
+
+        //(1 ponto)
+        //retorna o alimento preferido pequisado nos blocos 
+        public string GetAlimentoMaisPreferido()
+        {
+            // Cria um dicionário para contar a ocorrência de cada tipo de alimento.
+            var alimentoContagem = new Dictionary<string, int>();
+
+            // Percorre através de todos os blocos.
+            foreach (var bloco in Blocos)
             {
-                if (voo.AssentosOcupados < voo.TotalAssentos)
+                 // Verifica se o tipo de alimento já existe no dicionário (Dictionary) de contagem.
+                if (!alimentoContagem.ContainsKey(bloco.AlimentoPreferido))
                 {
-                    voo.AssentosOcupados++;
-                    var novaReserva = new Reserva(GetUniqueId(), passageiro.Id, voo.Id);
-                    Reservas.Add(novaReserva);
-                    break;
+                    alimentoContagem[bloco.AlimentoPreferido] = 1;
+                }
+                else
+                {
+                    alimentoContagem[bloco.AlimentoPreferido]++;
+                }
+            }
+
+            // Obtém o tipo de alimento preferido ao ordenar o dicionário por contagem decrescente(OrderByDescending)
+            // e pega o primeiro elemento (que será o tipo de alimento com a maior contagem na pesquisa dosblocos).
+            //kv de Keyvalue
+            var alimentoMaisPreferido = alimentoContagem.OrderByDescending(kv => kv.Value).FirstOrDefault().Key; 
+            //primeiro elemento na contagem mais alta(FirstOrDefault)
+            
+            return alimentoMaisPreferido;
+
+            //Retorna o alimento preferidop
+        }
+
+        //(2 pontos)
+        //aloca os quiosques nos blocos obedecendo as seguintes condições:
+        //- no máximo dois quiosques por bloco
+        //- o quiosque deve servir o alimento preferido do bloco
+        //- o bloco tem que estar aberto em todo horário de funcionamento do quiosque (mas não tem problema se o quiosque estiver fechado em parte do horário do bloco)
+        //(se quiser, faça funções separadas pra testar cada uma dessas condições)
+        //(se não conseguir testar a terceira condição, tente alocar corretamente seguindo pelo menos as duas primeiras)
+        //(não é necessário lançar exceção caso algum quiosque não possa ser alocado)
+        public void AlocarTodoMundo()
+        {
+            foreach (var bloco in Blocos)
+            {
+                var quiosquesDisponiveis = Quiosques
+                    .Where(q =>
+                        q.ServeSalgado == (bloco.AlimentoPreferido == "Salgado") &&
+                        q.Horario.Contains(bloco.Horario) &&
+                        !Alocacoes.Any(a => a.CorBloco == bloco.Cor && a.EmpresaQuiosque == q.Empresa))
+                    .ToList();
+                    //Verificando se o quiosque serve o alimento preferido do bloco
+
+
+                //para não yter mais que dois no mesmo bloco
+                if (quiosquesDisponiveis.Count >= 2)
+                {
+                    AlocarQuiosque(bloco, quiosquesDisponiveis[0]);
+                    AlocarQuiosque(bloco, quiosquesDisponiveis[1]); //V
+                }
+                else if (quiosquesDisponiveis.Count == 1)
+                {
+                    AlocarQuiosque(bloco, quiosquesDisponiveis[0]);
                 }
             }
         }
 
-        // Remova as reservas do voo cancelado
-        Reservas.RemoveAll(r => r.IdVoo == vooCancelado.Id);
+        private void AlocarQuiosque(Bloco bloco, Quiosque quiosque)
+        {
+            Alocacoes.Add(new Alocacao(quiosque.Empresa, bloco.Cor));
+            bloco.NumeroQuiosques++;
+        }
+    }
 
-        Console.WriteLine("Voo cancelado com sucesso!");
-    }
-    catch (Exception error)
+    class Program
     {
-        Console.WriteLine(error.Message);
+        static void Main(string[] args)
+        {
+            var controle = new Controle();
+
+            controle.Quiosques.Add(new Quiosque("Bobs", true, true, false, "Manha"));
+            controle.Quiosques.Add(new Quiosque("Burger King", true, true, false, "Ambos"));
+            controle.Quiosques.Add(new Quiosque("Cabra Café", true, false, false, "Manha"));
+            controle.Quiosques.Add(new Quiosque("Cacau Show", false, false, true, "Noite"));
+            controle.Quiosques.Add(new Quiosque("Freddo", false, false, true, "Manha"));
+            controle.Quiosques.Add(new Quiosque("Giraffas", true, true, false, "Manha"));
+            controle.Quiosques.Add(new Quiosque("McDonalds", true, true, false, "Ambos"));
+            controle.Quiosques.Add(new Quiosque("Pizza Hut", false, true, false, "Noite"));
+            controle.Quiosques.Add(new Quiosque("Ultra Coffee", true, false, true, "Ambos"));
+            controle.Quiosques.Add(new Quiosque("Zuka", false, false, true, "Noite"));
+
+            controle.Blocos.Add(new Bloco("Amarelo", "Salgado", "Ambos"));
+            controle.Blocos.Add(new Bloco("Azul", "Doce", "Noite"));
+            controle.Blocos.Add(new Bloco("Bege", "Salgado", "Noite"));
+            controle.Blocos.Add(new Bloco("Branco", "Salgado", "Manha"));
+            controle.Blocos.Add(new Bloco("Cinza", "Doce", "Ambos"));
+            controle.Blocos.Add(new Bloco("Laranja", "Salgado", "Ambos"));
+            controle.Blocos.Add(new Bloco("Marrom", "Salgado", "Manha"));
+            controle.Blocos.Add(new Bloco("Verde", "Bebida", "Manha"));
+            controle.Blocos.Add(new Bloco("Vermelho", "Doce", "Manha"));
+            controle.Blocos.Add(new Bloco("Roxo", "Bebida", "Noite"));
+
+            controle.AlocarTodoMundo();
+
+            Console.WriteLine("O numero de quiosques que abrem apenas a noite eh: " + controle.GetNumeroQuiosquesApenasNoite());
+            Console.WriteLine("Os quiosques que servem salgados pela manha sao: " + controle.GetEmpresasSalgadoManha());
+            Console.WriteLine("O tipo de alimento mais preferido pelos blocos eh: " + controle.GetAlimentoMaisPreferido());
+            Console.WriteLine("Lista de alocacoes:");
+
+            foreach (var alocacao in controle.Alocacoes)
+            {
+                Console.WriteLine("O quiosque " + alocacao.EmpresaQuiosque + " foi alocado no bloco " + alocacao.CorBloco);
+            }
+        }
     }
-		}
-	}
-	// coemntando para eu não bigodarrrr
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			try
-			{
-				Aeroporto aeroporto = new Aeroporto();
-				
-				aeroporto.CadastrarVoo(new DateTime(2022, 09, 26, 13, 0, 0), "Sao Paulo",      8);
-				aeroporto.CadastrarVoo(new DateTime(2022, 09, 26, 14, 0, 0), "Rio de Janeiro", 8);
-				aeroporto.CadastrarVoo(new DateTime(2022, 09, 26, 15, 0, 0), "Porto Alegre",   8);
-				aeroporto.CadastrarVoo(new DateTime(2022, 09, 26, 16, 0, 0), "Fortaleza",      8);
-				aeroporto.CadastrarVoo(new DateTime(2022, 09, 26, 17, 0, 0), "Manaus",         8);
-				aeroporto.CadastrarVoo(new DateTime(2022, 09, 27,  8, 0, 0), "Porto Alegre",   8);
-				aeroporto.CadastrarVoo(new DateTime(2022, 09, 27,  9, 0, 0), "Sao Paulo",      8);
-				aeroporto.CadastrarVoo(new DateTime(2022, 09, 27, 10, 0, 0), "Sao Paulo",      8);
-				aeroporto.CadastrarVoo(new DateTime(2022, 09, 27, 11, 0, 0), "Rio de Janeiro", 8);
-				aeroporto.CadastrarVoo(new DateTime(2022, 09, 27, 12, 0, 0), "Fortaleza",      8);
-				
-				aeroporto.CadastrarPassageiro("Aldair");
-				aeroporto.CadastrarPassageiro("Bebeto");
-				aeroporto.CadastrarPassageiro("Branco");
-				aeroporto.CadastrarPassageiro("Cafu");
-				aeroporto.CadastrarPassageiro("Parreira");
-				aeroporto.CadastrarPassageiro("Dunga");
-				aeroporto.CadastrarPassageiro("Gilmar");
-				aeroporto.CadastrarPassageiro("Jorginho");
-				aeroporto.CadastrarPassageiro("Junior");
-				aeroporto.CadastrarPassageiro("Leonardo");
-				aeroporto.CadastrarPassageiro("Marcio");
-				aeroporto.CadastrarPassageiro("Mauro");
-				aeroporto.CadastrarPassageiro("Mazinho");
-				aeroporto.CadastrarPassageiro("Muller");
-				aeroporto.CadastrarPassageiro("Paulo");
-				aeroporto.CadastrarPassageiro("Rai");
-				aeroporto.CadastrarPassageiro("Ricardo");
-				aeroporto.CadastrarPassageiro("Romario");
-				aeroporto.CadastrarPassageiro("Ronaldao");
-				aeroporto.CadastrarPassageiro("Ronaldo");
-				aeroporto.CadastrarPassageiro("Taffarel");
-				aeroporto.CadastrarPassageiro("Viola");
-				aeroporto.CadastrarPassageiro("Zagallo");
-				aeroporto.CadastrarPassageiro("Zetti");
-				aeroporto.CadastrarPassageiro("Zinho");
-				
-				aeroporto.ReservarVoo("Aldair",   new DateTime(2022, 09, 26, 14, 0, 0));
-				aeroporto.ReservarVoo("Bebeto",   new DateTime(2022, 09, 26, 13, 0, 0));
-				aeroporto.ReservarVoo("Branco",   new DateTime(2022, 09, 26, 13, 0, 0));
-				aeroporto.ReservarVoo("Cafu",     new DateTime(2022, 09, 26, 14, 0, 0));
-				aeroporto.ReservarVoo("Parreira", new DateTime(2022, 09, 26, 15, 0, 0));
-				aeroporto.ReservarVoo("Dunga",    new DateTime(2022, 09, 26, 14, 0, 0));
-				aeroporto.ReservarVoo("Gilmar",   new DateTime(2022, 09, 26, 15, 0, 0));
-				aeroporto.ReservarVoo("Jorginho", new DateTime(2022, 09, 26, 14, 0, 0));
-				aeroporto.ReservarVoo("Junior",   new DateTime(2022, 09, 26, 14, 0, 0));
-				aeroporto.ReservarVoo("Leonardo", new DateTime(2022, 09, 26, 14, 0, 0));
-				aeroporto.ReservarVoo("Marcio",   new DateTime(2022, 09, 26, 16, 0, 0));
-				aeroporto.ReservarVoo("Mauro",    new DateTime(2022, 09, 26, 13, 0, 0));
-				aeroporto.ReservarVoo("Mazinho",  new DateTime(2022, 09, 26, 13, 0, 0));
-				aeroporto.ReservarVoo("Muller",   new DateTime(2022, 09, 26, 13, 0, 0));
-				aeroporto.ReservarVoo("Paulo",    new DateTime(2022, 09, 26, 16, 0, 0));
-				aeroporto.ReservarVoo("Rai",      new DateTime(2022, 09, 26, 13, 0, 0));
-				aeroporto.ReservarVoo("Ricardo",  new DateTime(2022, 09, 27,  9, 0, 0));
-				aeroporto.ReservarVoo("Romario",  new DateTime(2022, 09, 26, 16, 0, 0));
-				aeroporto.ReservarVoo("Ronaldao", new DateTime(2022, 09, 26, 14, 0, 0));
-				aeroporto.ReservarVoo("Ronaldo",  new DateTime(2022, 09, 26, 13, 0, 0));
-				aeroporto.ReservarVoo("Taffarel", new DateTime(2022, 09, 26, 16, 0, 0));
-				aeroporto.ReservarVoo("Viola",    new DateTime(2022, 09, 26, 13, 0, 0));
-				aeroporto.ReservarVoo("Zagallo",  new DateTime(2022, 09, 26, 15, 0, 0));
-				aeroporto.ReservarVoo("Zetti",    new DateTime(2022, 09, 26, 16, 0, 0));
-				aeroporto.ReservarVoo("Zinho",    new DateTime(2022, 09, 27,  9, 0, 0));
-				
-				Console.WriteLine(aeroporto.ListarReservas());
-				
-				aeroporto.CancelarVoo(new DateTime(2022, 09, 26, 14, 0, 0));
-				aeroporto.CancelarVoo(new DateTime(2022, 09, 26, 13, 0, 0));
-				
-				Console.WriteLine(aeroporto.ListarReservas());
-			}
-			catch(Exception error)
-			{
-				Console.WriteLine(error.Message);
-			}
-		}
-	}
 }
